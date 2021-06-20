7207d24ca9855fd1be914de075cc9654c0f1893b
Iacopo454 <iacopo.gentile@icloud.com>
2021-06-20 21:49:41 +0000

    changes made to todo_list.html

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def get_todo_list(request):
-    return render('request', 'todo_list.html')
+    item = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render('request', 'todo_list.html', context)
 
